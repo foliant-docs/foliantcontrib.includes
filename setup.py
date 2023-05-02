cafcbf19871d063e7c53ec7bf8c08a249ada6f70
holamgadol <holamgadol@gmail.com>
2023-05-02 13:25:54 +0300

    update: bump version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     description=SHORT_DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     long_description_content_type='text/markdown',
-    version='1.1.15',
+    version='1.1.16',
     author='Konstantin Molchanov',
     author_email='moigagoo@live.com',
     url='https://github.com/foliant-docs/foliantcontrib.includes',
